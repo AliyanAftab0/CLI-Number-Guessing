330a3c40904751a993ac8ced071c99bc0e4495ee
AliyanAftab0 <159782573+AliyanAftab0@users.noreply.github.com>
2024-03-23 00:49:09 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! user/bin/node 
+
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 100 + 1);
+const answer = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Please guess a number between 1 to 100 : "
+},
+])
+
+if (answer.userGuessedNumber === randomnumber) {
+    console.log("Congratulations! you guessed right number.");
+} else {
+    console.log("You guessed wrong number!");  
+}
+console.log(randomnumber);
